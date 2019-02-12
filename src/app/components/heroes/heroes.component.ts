6a66dc75005d6669bc16569163a1353dc0d38153
vpellegrini <vanessa.pellegrini@telefonica.com>
2019-02-12 01:01:17 -0300

    se puede seleccionar cada heroe con su respectiva data

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  templateUrl: './heroes.component.html'
 })
 export class HeroesComponent implements OnInit {
 
   heroes: Heroe[] = [];
 
-  constructor(private heroesJSON: HeroesService) { }
+  constructor( private _heroesService: HeroesService,
+               private router: Router
+                ) {
+    // console.log("constructor");
+  }
 
   ngOnInit() {
-    this.heroes = this.heroesJSON.getHeroes();
-    console.log(this.heroes);
+    this.heroes = this._heroesService.getHeroes();
+    // console.log( this.heroes );
+  }
+
+  verHeroe( idx: number ) {
+    this.router.navigate( ['/heroe', idx] );
   }
 
 }
+
